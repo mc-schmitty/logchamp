5b995ecfce111a3c0858e1e50359e6d0f5d0ebc3
Quinn Fisher <quinnfisher@hotmail.com>
2021-04-05 22:05:29 -0400

    Added SFX to boss level and implemented level progression

--- Assets/Scripts/TruckBehaviour.cs
+++ Assets/Scripts/TruckBehaviour.cs
@@ -7,31 +7,45 @@ public class TruckBehaviour : MonoBehaviour
     [SerializeField] private bool isCrashed;
     [SerializeField] private float speed;
 
+    AudioSource[] truckSounds;
+
     // Start is called before the first frame update
     void Start()
     {
         isCrashed = false;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        truckSounds = GetComponentsInChildren<AudioSource>();
     }
 
     public void Drive() {
         GetComponent<Rigidbody>().AddForce(Vector3.left * speed, ForceMode.VelocityChange);
+        truckSounds[0].Play();
     }
 
     private void OnTriggerEnter(Collider other) {
-        if(other.CompareTag("Stopper"))
+        if (other.CompareTag("Stopper"))
+        {
             GetComponent<Rigidbody>().AddForce(Vector3.right * speed, ForceMode.VelocityChange);
-        else if(other.CompareTag("Hazard") && !isCrashed)
+            
+        }
+
+        else if (other.CompareTag("Hazard") && !isCrashed)
+        {
             Crash();
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.gameObject.layer == 10)
+        {
+            truckSounds[1].Play();
+            truckSounds[2].Play();
+        }
     }
 
     // Sink truck into ground
     private void Crash() {
+        truckSounds[3].Play();
         isCrashed = true;
         GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
         GetComponent<BoxCollider>().enabled = false;

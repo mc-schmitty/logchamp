a397a8f16ba41b407ba8da2d4d604f3aeb263528
Quinn Fisher <quinnfisher@hotmail.com>
2021-04-05 20:50:06 -0400

    Adjusted main menu, added target SFX

--- Assets/Scripts/TargetBase.cs
+++ Assets/Scripts/TargetBase.cs
@@ -6,17 +6,14 @@ public class TargetBase : MonoBehaviour
 {
     protected bool hasHead;
     [SerializeField] private int team;
+    AudioSource sfx;
 
     // Start is called before the first frame update
     void Start()
     {
         hasHead = true;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        sfx = GetComponent<AudioSource>();
+        sfx.pitch = (Random.Range(0.7f, 1.2f));
     }
 
     // Return team int
@@ -30,6 +27,7 @@ public class TargetBase : MonoBehaviour
         if(hasHead) {
             hasHead = false;
             GetComponentInParent<TargetManager>().LostHead(team);
+            sfx.Play();
         }
     }
 }

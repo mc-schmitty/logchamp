11aa451ad52669fb711f021da8de37613b98f7cc
mc-schmitty <81720749+mc-schmitty@users.noreply.github.com>
2021-04-05 20:53:05 -0400

    Updated shooting object, scripting
    it dont shoot tho

--- Assets/Scripts/Conveyor.cs
+++ Assets/Scripts/Conveyor.cs
@@ -6,10 +6,20 @@ public class Conveyor : MonoBehaviour
 {
     private bool activated;
     private int shootCooldown;
+    [SerializeField] private GameObject[] nodes;
+    private int nextNode;
+    [SerializeField] private GameObject regLog;
+    [SerializeField] private GameObject bombLog;
+    [SerializeField] private float launchSpeed;
+    private Vector3 queueDir;
+    private GameObject queueLog = null;
+    private Vector3 readyDir;
+    private GameObject readyLog = null;
 
     // Start is called before the first frame update
     void Start()
     {
+        nextNode = 0;
         activated = false;
         shootCooldown = 120;
     }
@@ -25,11 +35,47 @@ public class Conveyor : MonoBehaviour
         if(!activated)
             return;
         
-        
+        if(shootCooldown == 0){
+            float randResult = Random.value - 0.7f;
+            Ready(nextNode, randResult);
+            if(nextNode < nodes.Length-1)
+                nextNode++;
+            else
+                nextNode = 0;
+        }
+        shootCooldown--;
     }
 
     public void activate() {
-        transform.Translate(2, 0, 0);
+        transform.Translate(4, 0, 0);
         activated = true;
+    }
+
+    private void Ready(int index, float logType) {
+        Vector3 pos = transform.position;
+        pos.Set(pos.x, pos.y+2, pos.z);
+
+        Shoot();
+
+        if(logType > 0.5)
+            // Create new log at end of queue
+            queueLog = Instantiate(bombLog, pos, transform.rotation);
+            queueDir = (nodes[index].transform.position - pos);
+            queueDir.y = 0;
+            queueDir = queueDir.normalized * launchSpeed;
+        
+        shootCooldown = 90;
+    }
+
+    private void Shoot() {
+        if(!readyLog) {
+            readyLog.GetComponent<Rigidbody>().AddForce(readyDir, ForceMode.VelocityChange);
+        }
+
+        if(!queueLog) {
+            readyLog = queueLog;
+            readyDir = queueDir;
+            readyLog.transform.Translate(3, 0, 0);
+        }
     }
 }
